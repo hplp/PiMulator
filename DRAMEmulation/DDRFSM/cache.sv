efe3def040cae6363bb4cf7bec11e81f999a3537
Sergiu Mosanu <sm7ed@virginia.edu>
2020-10-06 19:26:10 -0400

    implement write cache FSM

--- DRAMEmulation/DDRFSM/cache.sv
+++ DRAMEmulation/DDRFSM/cache.sv
@@ -1,34 +1,55 @@
-// Created by fizzim.pl version 5.20 on 2020:10:05 at 18:44:10 (www.fizzim.com)
+// Created by fizzim.pl version 5.20 on 2020:10:06 at 16:33:52 (www.fizzim.com)
 
-module cache (
-  output logic Check,
-  output logic [4:0] cRowId,
+module cache #(
+  parameter CHWIDTH = 5,
+  parameter ADDRWIDTH = 17,
+  
+  localparam CHROWS = 2**CHWIDTH,
+  localparam ROWS = 2**ADDRWIDTH
+  )
+  (
+  output logic [CHWIDTH-1:0] cRowId,
   output logic hold,
-  input logic MemOK,
   input logic RD,
-  input logic [16:0] RowId,
+  input logic [ADDRWIDTH-1:0] RowId,
   input logic WR,
   input logic clk,
-  input logic rst
+  input logic rst,
+  input logic sync
   );
   
   typedef struct packed {
     logic valid;
     logic dirty;
-    logic [5-1:0] tag;
-    logic [17-1:0] rowaddr;
+    logic [CHWIDTH-1:0] age; // for LRU
+    logic [CHWIDTH-1:0] tag;
+    logic [ADDRWIDTH-1:0] rowaddr;
     logic [64-1:0] addr;
-  }cache_tag_type;
+  } cache_tag_type;
   
-  cache_tag_type cache_tag [0:32-1];
-  logic hit = 1;
+  cache_tag_type cache_tag [0:CHROWS-1];
+  genvar idx;
+  generate
+    for (idx = 0; idx < CHROWS; idx++) begin
+      initial cache_tag[idx].valid=0;
+      initial cache_tag[idx].dirty=0;
+      initial cache_tag[idx].age='0;
+      initial cache_tag[idx].tag=idx;
+      initial cache_tag[idx].rowaddr='0;
+      initial cache_tag[idx].addr='0;
+    end
+  endgenerate
+  logic hit=0, miss=0, newrow=0;
   
   // state bits
-  enum logic [1:0] {
-    Idle     = 2'b00, 
-    CheckRow = 2'b01, 
-    Miss     = 2'b10, 
-    RDWR     = 2'b11
+  enum logic [2:0] {
+    Idle    = 3'b000, 
+    RDCheck = 3'b001, 
+    RDMiss  = 3'b010, 
+    READ    = 3'b011, 
+    WRCheck = 3'b100, 
+    WRITE   = 3'b101, 
+    WRMiss  = 3'b110
   } state, nextstate;
   
   
@@ -36,36 +57,57 @@ module cache (
   always_comb begin
     nextstate = state; // default to hold value because implied_loopback is set
     case (state)
-      Idle    : begin
-        if (RD || WR) begin
-          nextstate = CheckRow;
+      Idle   : begin
+        if (WR) begin
+          nextstate = WRCheck;
+        end
+        else if (RD) begin
+          nextstate = RDCheck;
         end
         else begin
           nextstate = Idle;
         end
       end
-      CheckRow: begin // row valid or not
-        if (hit) begin
-          nextstate = RDWR;
+      RDCheck: begin
+        if (miss) begin
+          nextstate = RDMiss;
         end
-        else if (!hit) begin
-          nextstate = Miss;
+        else if (hit) begin
+          nextstate = READ;
         end
       end
-      Miss    : begin
-        if (MemOK) begin
-          nextstate = RDWR;
+      RDMiss : begin
+        if (sync) begin
+          nextstate = READ;
         end
         else begin
-          nextstate = Miss;
+          nextstate = RDMiss;
         end
       end
-      RDWR    : begin
-        if (!(RD || WR)) begin
+      READ   : begin
+        begin
           nextstate = Idle;
         end
+      end
+      WRCheck: begin
+        if (miss) begin
+          nextstate = WRMiss;
+        end
+        else if (hit) begin
+          nextstate = WRITE;
+        end
+      end
+      WRITE  : begin
+        begin
+          nextstate = Idle;
+        end
+      end
+      WRMiss : begin
+        if (sync) begin
+          nextstate = WRITE;
+        end
         else begin
-          nextstate = RDWR;
+          nextstate = WRMiss;
         end
       end
     endcase
@@ -84,24 +126,55 @@ module cache (
   // datapath sequential always block
   always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
-      Check <= 0;
       cRowId[4:0] <= 5'b0;
       hold <= 0;
     end
     else begin
-      Check <= 0; // default
       cRowId[4:0] <= 5'b0; // default
       hold <= 0; // default
       case (nextstate)
-        CheckRow: begin
-          Check <= 1;
-          for (int i = 0; i < 32; i++) begin
-            if(RowId == cache_tag[i].rowaddr)
-            cRowId = cache_tag[i].tag;
+        Idle   : begin
+          hit <= 0;
+          newrow <= 0;
+        end
+        RDCheck: begin
+          
+        end
+        RDMiss : begin
+          hold <= 1;
+        end
+        READ   : begin
+          
+        end
+        WRCheck: begin
+          if(newrow) begin
+            for (int i=CHROWS; i>0; i--) begin
+              if(!cache_tag[i-1].valid) begin
+                cRowId <= cache_tag[i-1].tag;
+                hit <= 1;
+              end
+            end
+          end
+          else
+          for (int i = 0; i < CHROWS; i++) begin
+            if((RowId == cache_tag[i].rowaddr) && (cache_tag[i].valid)) begin
+              cRowId <= cache_tag[i].tag;
+              hit <= 1;
+            end
+            else
+            newrow <= 1;
           end
         end
-        Miss    : begin
+        WRMiss : begin
           hold <= 1;
+        end
+        WRITE  : begin
+          for (int i = 0; i < CHROWS; i++) begin
+            if(cRowId == cache_tag[i].tag) begin
+              cache_tag[i].valid <= 1;
+              cache_tag[i].rowaddr <= RowId;
+            end
+          end
         end
       endcase
     end
--- DRAMEmulation/DDRFSM/cachetestbench.sv
+++ DRAMEmulation/DDRFSM/cachetestbench.sv
@@ -3,35 +3,40 @@
 module cachetestbench(
        );
        
+       parameter CHWIDTH = 5;
        parameter ADDRWIDTH = 17;
-       parameter COLWIDTH = 10;
-       parameter DEVICE_WIDTH = 4; // x4, x8, x16 -> DQ width = Device_Width x BankGroups (Chips)
-       parameter BL = 8; // Burst Length
+       // parameter COLWIDTH = 10;
+       // parameter DEVICE_WIDTH = 4; // x4, x8, x16 -> DQ width = Device_Width x BankGroups (Chips)
+       // parameter BL = 8; // Burst Length
        
+       localparam CHROWS = 2**CHWIDTH;
        localparam ROWS = 2**ADDRWIDTH;
-       localparam COLS = 2**COLWIDTH;
+       // localparam COLS = 2**COLWIDTH;
        
        localparam tCK = 0.75;
        
-       reg [4:0] cRowId;
+       reg [CHWIDTH-1:0] cRowId;
        reg hold;
-       reg MemOK;
        reg RD;
-       reg [16:0] RowId;
+       reg [ADDRWIDTH-1:0] RowId;
        reg WR;
        reg clk;
-       reg rst;       
+       reg rst;
+       reg sync;
        
-       cache //#(.RANKS(RANKS),) 
+       cache #(
+       .CHWIDTH(CHWIDTH),
+       .ADDRWIDTH(ADDRWIDTH)
+       ) 
        dut (
        .cRowId(cRowId),
        .hold(hold),
-       .MemOK(MemOK),
        .RD(RD),
        .RowId(RowId),
        .WR(WR),
        .clk(clk),
-       .rst(rst)
+       .rst(rst),
+       .sync(sync)
        );
        
        always #(tCK*0.5) clk = ~clk;
@@ -42,7 +47,7 @@ module cachetestbench(
        begin
               rst = 1;
               clk = 1;
-              MemOK = 0;
+              sync = 0;
               RD = 0;
               WR=0;
               RowId=0;
@@ -51,10 +56,24 @@ module cachetestbench(
               rst = 0;
               #tCK
               
+              // write
               WR=1;
               RowId=150;
               #tCK
+              #tCK
+              #tCK
               
+              WR=0;
+              RowId=0;
+              #tCK
+
+              // write
+              WR=1;
+              RowId=590;
+              #tCK
+              #tCK
+              #tCK
+
               WR=0;
               RowId=0;
               #tCK
