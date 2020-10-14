af7f3329041b25e4ec7a6e1659f4730b4d58d79b
Sergiu Mosanu <sm7ed@virginia.edu>
2020-10-14 14:54:37 -0400

    un-ignore files

---
+++ DRAMEmulation/DDRFSM/tristate.v
@@ -0,0 +1,15 @@
+module tristate #(
+         parameter W=8
+       )
+       (
+         input [W-1:0]dqi,
+         output [W-1:0]dqo,
+         inout [W-1:0]dq,
+         input enable
+       );
+
+
+assign dq = (enable) ? dqi : {W{1'bZ}};
+assign dqo = (enable) ? {W{1'b0}} : dq;
+
+endmodule
